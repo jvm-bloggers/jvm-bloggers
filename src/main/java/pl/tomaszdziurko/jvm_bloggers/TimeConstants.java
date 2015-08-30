719a5d43f392af5e11b8151254e55cf7c83fdd2f
Tomasz Dziurko <tdziurko@gmail.com>
2015-08-30 12:18:53 +0200

    TimeConstants refactored

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/TimeConstants.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/TimeConstants.java
@@ -2,5 +2,7 @@ package pl.tomaszdziurko.jvm_bloggers;
 
 public class TimeConstants {
 
-    public static final long THIRTY_MINUTES = 30 * 60 * 1000L;
+    public static final long ONE_MINUTE = 60 * 1000L;
+    public static final long THIRTY_MINUTES = 30 * ONE_MINUTE;
+    public static final long ONE_HOUR = 60 * ONE_MINUTE;
 }
