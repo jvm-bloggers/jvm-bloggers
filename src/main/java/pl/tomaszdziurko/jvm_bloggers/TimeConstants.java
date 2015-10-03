3591c26abba9c249dca7830948530825595c47bc
Tomasz Dziurko <tdziurko@gmail.com>
2015-10-03 12:57:55 +0200

    Remove Heroku pinging scheduler, now http://kaffeine.herokuapp.com/ is responsible for keeping application awake

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/TimeConstants.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/TimeConstants.java
@@ -5,7 +5,6 @@ public class TimeConstants {
     public static final String FIVE_MINUTES_AFTER_EACH_HOUR = "0 5 * * * *";
     public static final String TEN_MINUTES_AFTER_EACH_HOUR = "0 10 * * * *";
 
-    public static final String EVERY_55TH_MINUTE_OF_EACH_HOUR_BETWEEN_8_AND_23 = "0 55 8-22 * * *";
     public static final String EVERY_FRIDAY_AT_12_OCLOCK = "0 0 12 * * FRI";
 
 }
