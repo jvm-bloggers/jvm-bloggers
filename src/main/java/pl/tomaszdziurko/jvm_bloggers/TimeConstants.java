7175d0a89405033523ffdb1d33c2f563b603810b
Tomasz Dziurko <tdziurko@gmail.com>
2015-10-25 19:12:25 +0100

    Return time when newsletter is sent to the original 12am

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/TimeConstants.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/TimeConstants.java
@@ -5,6 +5,6 @@ public class TimeConstants {
     public static final String FIVE_MINUTES_AFTER_EACH_HOUR = "0 5 * * * *";
     public static final String TEN_MINUTES_AFTER_EACH_HOUR = "0 10 * * * *";
 
-    public static final String EVERY_FRIDAY_AT_12_OCLOCK = "0 0 13 * * FRI";
+    public static final String EVERY_FRIDAY_AT_12_OCLOCK = "0 0 12 * * FRI";
 
 }
