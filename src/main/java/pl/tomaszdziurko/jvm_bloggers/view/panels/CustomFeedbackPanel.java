93b417713c87adbc70b0351481a07aaf73ce5022
Tomasz Dziurko <tdziurko@gmail.com>
2016-02-06 11:38:58 +0100

    Adds custom Wicket FeedbackPanel

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/view/panels/CustomFeedbackPanel.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/view/panels/CustomFeedbackPanel.java
@@ -1,4 +1,24 @@
 package pl.tomaszdziurko.jvm_bloggers.view.panels;
 
-public class CustomFeedbackPanel {
+import org.apache.wicket.feedback.FeedbackMessage;
+import org.apache.wicket.feedback.IFeedbackMessageFilter;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+
+public class CustomFeedbackPanel extends FeedbackPanel {
+
+    final static String COMMON_FEEDBACK_CSS_CLASS = "feedbackPanelItem";
+
+    public CustomFeedbackPanel(String id) {
+        super(id);
+    }
+
+    public CustomFeedbackPanel(String id, IFeedbackMessageFilter filter) {
+        super(id, filter);
+    }
+
+
+    @Override
+    protected String getCSSClass(FeedbackMessage message) {
+        return COMMON_FEEDBACK_CSS_CLASS + " " + super.getCSSClass(message);
+    }
 }
