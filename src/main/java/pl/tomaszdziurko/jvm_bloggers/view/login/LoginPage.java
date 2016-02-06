70aaccfc4e3fba05a06907b2769d4e5b82e514ef
Tomasz Dziurko <tdziurko@gmail.com>
2016-02-06 11:47:01 +0100

    Simple LoginPage

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/view/login/LoginPage.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/view/login/LoginPage.java
@@ -1,18 +1,38 @@
 package pl.tomaszdziurko.jvm_bloggers.view.login;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.wicket.devutils.stateless.StatelessComponent;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.model.ComponentModel;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.model.CompoundPropertyModel;
 import pl.tomaszdziurko.jvm_bloggers.view.BasePage;
+import pl.tomaszdziurko.jvm_bloggers.view.panels.CustomFeedbackPanel;
 
 @StatelessComponent
+@Slf4j
 public class LoginPage extends BasePage {
 
     private String login;
     private String password;
 
     public LoginPage() {
-
-        Form<Void> loginForm = new Form<Void>("loginForm", new ComponentModel<LoginPage>());
+        StatelessForm<LoginPage> loginForm = new StatelessForm<LoginPage>("loginForm", new CompoundPropertyModel<>(this)) {
+            @Override
+            protected void onSubmit() {
+                log.info("Logging using " + login + " and " + password);
+                success("Success!");
+            }
+        };
+        CustomFeedbackPanel feedbackPanel = new CustomFeedbackPanel("feedbackPanel");
+        loginForm.add(feedbackPanel);
+        RequiredTextField loginField = new RequiredTextField("login");
+        loginForm.add(loginField);
+        PasswordTextField passwordField = new PasswordTextField("password");
+        loginForm.add(passwordField);
+        Button loginButton = new Button("submit");
+        loginForm.add(loginButton);
+        add(loginForm);
     }
 }
