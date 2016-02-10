177fd5a7c7e0436319b4fc8983eb9456e026f928
Tomasz Dziurko <tdziurko@gmail.com>
2016-02-10 22:48:55 +0100

    Some more Wicket debug stuff is now enabled in dev profile

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/view/admin/AbstractAdminPage.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/view/admin/AbstractAdminPage.java
@@ -1,9 +1,11 @@
 package pl.tomaszdziurko.jvm_bloggers.view.admin;
 
+import org.apache.wicket.devutils.debugbar.DebugBar;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import pl.tomaszdziurko.jvm_bloggers.view.admin.mailing.MailingPage;
+import pl.tomaszdziurko.jvm_bloggers.view.admin.moderation.ModerationPage;
 
 public abstract class AbstractAdminPage extends WebPage {
 
@@ -24,5 +26,7 @@ public abstract class AbstractAdminPage extends WebPage {
         add(new BookmarkablePageLink<AdminDashboardPage>("adminHomePage", AdminDashboardPage.class));
         add(new BookmarkablePageLink<AdminDashboardPage>("dashboardLink", AdminDashboardPage.class));
         add(new BookmarkablePageLink<AdminDashboardPage>("mailingLink", MailingPage.class));
-    }
+        add(new BookmarkablePageLink<AdminDashboardPage>("moderationLink", ModerationPage.class));
+
+        add(new DebugBar("debug"));    }
 }
