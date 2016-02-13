f5932f2e5c1055400a5223d89bfe13deefedbfdd
Tomasz Dziurko <tdziurko@gmail.com>
2016-02-13 20:49:02 +0100

    LoginPage uses continueToOriginalDestination when redirecting after successful login, #85, some tests for LoginPage

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/view/login/LoginPage.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/view/login/LoginPage.java
@@ -1,6 +1,7 @@
 package pl.tomaszdziurko.jvm_bloggers.view.login;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.wicket.RestartResponseAtInterceptPageException;
 import org.apache.wicket.authroles.authorization.strategies.role.Roles;
 import org.apache.wicket.devutils.stateless.StatelessComponent;
 import org.apache.wicket.markup.html.WebPage;
@@ -10,12 +11,18 @@ import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import pl.tomaszdziurko.jvm_bloggers.view.admin.AdminDashboardPage;
 import pl.tomaszdziurko.jvm_bloggers.view.panels.CustomFeedbackPanel;
 import pl.tomaszdziurko.jvm_bloggers.view.session.UserSession;
 
 @StatelessComponent
 @Slf4j
 public class LoginPage extends WebPage {
+
+    public static final String LOGIN_FORM_ID = "loginForm";
+    public static final String LOGIN_FIELD_ID = "login";
+    public static final String PASSWORD_FIELD_ID = "password";
+    public static final String FORM_SUBMIT_ID = "submit";
 
     private String login;
     private String password;
@@ -24,7 +31,7 @@ public class LoginPage extends WebPage {
     private UserAuthenticator userAuthenticator;
 
     public LoginPage() {
-        StatelessForm<LoginPage> loginForm = new StatelessForm<LoginPage>("loginForm", new CompoundPropertyModel<>(this)) {
+        StatelessForm<LoginPage> loginForm = new StatelessForm<LoginPage>(LOGIN_FORM_ID, new CompoundPropertyModel<>(this)) {
             @Override
             protected void onSubmit() {
                 log.info("Login attempt as user " + login);
@@ -32,7 +39,11 @@ public class LoginPage extends WebPage {
 
                 if (roles.hasRole(Roles.ADMIN)) {
                     UserSession.get().loginAs(login, roles);
-                    continueToOriginalDestination();
+                    if (RestartResponseAtInterceptPageException.getOriginalUrl() != null) {
+                        continueToOriginalDestination();
+                    } else {
+                        setResponsePage(AdminDashboardPage.class);
+                    }
                 } else {
                     log.warn("Invalid login credentials");
                     error("Incorrect login or password");
@@ -41,11 +52,11 @@ public class LoginPage extends WebPage {
         };
         CustomFeedbackPanel feedbackPanel = new CustomFeedbackPanel("feedbackPanel");
         loginForm.add(feedbackPanel);
-        RequiredTextField loginField = new RequiredTextField("login");
+        RequiredTextField loginField = new RequiredTextField(LOGIN_FIELD_ID);
         loginForm.add(loginField);
-        PasswordTextField passwordField = new PasswordTextField("password");
+        PasswordTextField passwordField = new PasswordTextField(PASSWORD_FIELD_ID);
         loginForm.add(passwordField);
-        Button loginButton = new Button("submit");
+        Button loginButton =  new Button(FORM_SUBMIT_ID);
         loginForm.add(loginButton);
         add(loginForm);
     }
---
+++ src/test/groovy/pl/tomaszdziurko/jvm_bloggers/view/login/LoginPageSpec.groovy
@@ -0,0 +1,62 @@
+package pl.tomaszdziurko.jvm_bloggers.view.login
+
+import org.apache.wicket.protocol.http.WebApplication
+import org.apache.wicket.util.tester.FormTester
+import org.apache.wicket.util.tester.WicketTester
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.util.ReflectionTestUtils
+import pl.tomaszdziurko.jvm_bloggers.JvmBloggersApplication
+import pl.tomaszdziurko.jvm_bloggers.view.admin.AdminDashboardPage
+import spock.lang.Specification
+
+@ContextConfiguration(classes = [JvmBloggersApplication], loader = SpringApplicationContextLoader)
+class LoginPageSpec extends Specification {
+
+    static String PASSWORD = "superSecretPassword"
+
+    private WicketTester tester
+
+    @Autowired
+    private WebApplication wicketApplication
+
+    def setupSpec() {
+        System.setProperty("jasypt.encryptor.password", PASSWORD);
+    }
+
+    def setup() {
+        // This is a workaround for problems with Spring Boot and WicketTester
+        // https://issues.apache.org/jira/browse/WICKET-6053 and
+        // https://github.com/MarcGiffing/wicket-spring-boot/issues/31
+        ReflectionTestUtils.setField(wicketApplication, "name", null)
+        tester = new WicketTester(wicketApplication)
+    }
+
+    def "Should redirect to Admin Dashboard after successful login"() {
+        when:
+            tester.startPage(LoginPage.class)
+            FormTester formTester = tester.newFormTester(LoginPage.LOGIN_FORM_ID)
+            formTester.setValue(LoginPage.LOGIN_FIELD_ID, "Any User")
+            formTester.setValue(LoginPage.PASSWORD_FIELD_ID, PASSWORD)
+            formTester.submit(LoginPage.FORM_SUBMIT_ID)
+        then:
+            tester.assertNoErrorMessage()
+            tester.assertRenderedPage(AdminDashboardPage.class)
+    }
+
+    def "Should reject incorrect password"() {
+        when:
+            tester.startPage(LoginPage.class)
+            FormTester formTester = tester.newFormTester(LoginPage.LOGIN_FORM_ID)
+            formTester.setValue(LoginPage.LOGIN_FIELD_ID, "Any User")
+            formTester.setValue(LoginPage.PASSWORD_FIELD_ID, "incorrect password")
+            formTester.submit(LoginPage.FORM_SUBMIT_ID)
+        then:
+            tester.assertErrorMessages("Incorrect login or password")
+            tester.assertRenderedPage(LoginPage.class)
+    }
+
+
+
+}
