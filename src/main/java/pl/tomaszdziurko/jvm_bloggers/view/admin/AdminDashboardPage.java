b289a33cda6909cbc6d80f698d2824696cae8f41
Mateusz Urbański <matek2305@gmail.com>
2016-03-05 21:50:05 +0100

    fix after refactor

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/view/admin/AdminDashboardPage.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/view/admin/AdminDashboardPage.java
@@ -3,12 +3,13 @@ package pl.tomaszdziurko.jvm_bloggers.view.admin;
 import org.apache.wicket.authroles.authorization.strategies.role.Roles;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.basic.Label;
+import pl.tomaszdziurko.jvm_bloggers.view.admin.counters.NewPostsCounterModel;
 
 @AuthorizeInstantiation(Roles.ADMIN)
 public class AdminDashboardPage extends AbstractAdminPage {
 
     public AdminDashboardPage() {
-        Label postsSinceLastPublicationLabel = new Label("postsSinceLastPublication", new DashboardNewPostsCounterModel());
+        Label postsSinceLastPublicationLabel = new Label("postsSinceLastPublication", new NewPostsCounterModel());
         add(postsSinceLastPublicationLabel);
     }
 }
