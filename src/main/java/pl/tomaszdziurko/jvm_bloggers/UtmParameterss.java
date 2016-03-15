3094f688fbda742fc9431208e79e65ff469ccb44
Tomasz Dziurko <tdziurko@gmail.com>
2016-03-15 08:18:52 +0100

    Fix case-insensitive issues on OS X, part 1

---
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/UtmParameterss.java
@@ -0,0 +1,16 @@
+package pl.tomaszdziurko.jvm_bloggers;
+
+/**
+ * UTM (Urchin Traffic Monitor) URL query parameter identifiers.
+ * 
+ * @see <a href="https://en.wikipedia.org/wiki/UTM_parameters">UTM Parameters</a>
+ * @author cslysy <jakub.sprega@gmail.com>
+ */
+public class UtmParameterss {
+    
+    public static final String UTM_SOURCE_KEY = "utm_source";
+    
+    public static final String UTM_MEDIUM_KEY = "utm_medium";
+    
+    public static final String UTM_CAMPAIGN_KEY = "utm_campaign";
+}
