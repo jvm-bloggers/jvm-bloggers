b742a0cbf4281169627f87209223478d98939fb9
Jonatan Borkowski <jonatanborkowski@wp.pl>
2016-03-15 11:04:11 +0100

    #127 add RequiredArgsConstructor annotation

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/blog_posts/BlogPostsFetchingScheduler.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/blog_posts/BlogPostsFetchingScheduler.java
@@ -1,6 +1,7 @@
 package pl.tomaszdziurko.jvm_bloggers.blog_posts;
 
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,26 +14,19 @@ import pl.tomaszdziurko.jvm_bloggers.utils.NowProvider;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class BlogPostsFetchingScheduler {
 
     private final BlogPostsFetcher blogPostsFetcher;
     private final MetadataRepository metadataRepository;
     private final NowProvider nowProvider;
 
-    @Autowired
-    public BlogPostsFetchingScheduler(BlogPostsFetcher blogPostsFetcher,
-                                      MetadataRepository metadataRepository,
-                                      NowProvider nowProvider) {
-        this.blogPostsFetcher = blogPostsFetcher;
-        this.metadataRepository = metadataRepository;
-        this.nowProvider = nowProvider;
-    }
-
     @Scheduled(cron = TimeConstants.EVERY_SIX_HOURS)
     public void checkRssForNewBlogPosts() {
         log.info("Starting scheduler: fetching blog posts");
         blogPostsFetcher.refreshPosts();
-        final Metadata dateOfLastFetch = metadataRepository.findByName(MetadataKeys.DATE_OF_LAST_FETCHING_BLOG_POSTS.toString());
+        final Metadata dateOfLastFetch = metadataRepository
+                .findByName(MetadataKeys.DATE_OF_LAST_FETCHING_BLOG_POSTS.toString());
         dateOfLastFetch.setValue(nowProvider.now().toString());
         metadataRepository.save(dateOfLastFetch);
     }
--- src/main/java/pl/tomaszdziurko/jvm_bloggers/blogs/BloggersDataFetchingScheduler.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/blogs/BloggersDataFetchingScheduler.java
@@ -1,6 +1,7 @@
 package pl.tomaszdziurko.jvm_bloggers.blogs;
 
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,26 +14,19 @@ import pl.tomaszdziurko.jvm_bloggers.utils.NowProvider;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class BloggersDataFetchingScheduler {
 
     private final BloggersDataFetcher bloggersDataFetcher;
     private final MetadataRepository metadataRepository;
     private final NowProvider nowProvider;
 
-    @Autowired
-    public BloggersDataFetchingScheduler(BloggersDataFetcher bloggersDataFetcher,
-                                         MetadataRepository metadataRepository,
-                                         NowProvider nowProvider) {
-        this.bloggersDataFetcher = bloggersDataFetcher;
-        this.metadataRepository = metadataRepository;
-        this.nowProvider = nowProvider;
-    }
-
     @Scheduled(cron = TimeConstants.EVERY_EIGHT_AM_AND_PM)
     public void fetchBloggersData() {
         log.info("Starting scheduler: bloggers data refresh");
         bloggersDataFetcher.refreshData();
-        final Metadata dateOfLastFetch = metadataRepository.findByName(MetadataKeys.DATE_OF_LAST_FETCHING_BLOGGERS.toString());
+        final Metadata dateOfLastFetch = metadataRepository
+                .findByName(MetadataKeys.DATE_OF_LAST_FETCHING_BLOGGERS.toString());
         dateOfLastFetch.setValue(nowProvider.now().toString());
         metadataRepository.save(dateOfLastFetch);
     }
