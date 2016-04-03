280e706f19b45a79d03c27a1a2a5234baf294340
Tomasz Dziurko <tdziurko@gmail.com>
2016-04-03 18:06:08 +0200

    Create simpler tests for CustomPagingNavigator

--- src/test/groovy/pl/tomaszdziurko/jvm_bloggers/view/login/CustomPagingNavigatorSpec.groovy
+++ src/test/groovy/pl/tomaszdziurko/jvm_bloggers/view/login/CustomPagingNavigatorSpec.groovy
@@ -1,47 +1,44 @@
 package pl.tomaszdziurko.jvm_bloggers.view.login
 
+import org.apache.wicket.AttributeModifier
 import org.apache.wicket.Component
-import org.apache.wicket.markup.repeater.data.DataView
+import org.apache.wicket.markup.html.navigation.paging.IPageable
 import org.apache.wicket.model.Model
-import org.apache.wicket.protocol.http.WebApplication
-import org.apache.wicket.util.tester.WicketTester
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.util.ReflectionTestUtils
-import pl.tomaszdziurko.jvm_bloggers.SpringContextAwareSpecification
-import pl.tomaszdziurko.jvm_bloggers.view.login.helpers.TestDataNavigationPage
+import pl.tomaszdziurko.jvm_bloggers.MockSpringContextAwareSpecification
+import pl.tomaszdziurko.jvm_bloggers.view.panels.CustomPagingNavigator
 
-class CustomPagingNavigatorSpec extends SpringContextAwareSpecification {
+class CustomPagingNavigatorSpec extends MockSpringContextAwareSpecification {
 
-    private WicketTester tester
-
-    DataView dataView = Mock(DataView)
-
-    @Autowired
-    private WebApplication wicketApplication
-
-    def setup() {
-        // This is a workaround for problems with Spring Boot and WicketTester
-        // https://issues.apache.org/jira/browse/WICKET-6053 and
-        // https://github.com/MarcGiffing/wicket-spring-boot/issues/31
-        ReflectionTestUtils.setField(wicketApplication, "name", null)
-        tester = new WicketTester(wicketApplication)
-    }
-
-    def "Should contain label with total numbers of pages"() {
+    def "Should render a label with total numbers of pages"() {
+        given:
+            int numberOfPages = 10
+            IPageable pageable = Stub(IPageable)
+            pageable.getPageCount() >> numberOfPages
+            pageable.getCurrentPage() >> 3
+            CustomPagingNavigator customPaging = new CustomPagingNavigator("someId", pageable)
         when:
-            tester.startPage(new TestDataNavigationPage(10, 100))
+            tester.startComponentInPage(customPaging)
         then:
-            tester.assertVisible("navigator:total")
-            tester.assertLabel("navigator:total", "Total pages: 10")
+            tester.assertVisible("someId:total")
+            tester.assertLabel("someId:total", "Total pages: $numberOfPages")
     }
 
-    def "Should navigate current page active"() {
+    def "Should mark button for current page as active"() {
+        given:
+            int currentPage = 3
+            IPageable pageable = Stub(IPageable)
+            pageable.getPageCount() >> 10
+            pageable.getCurrentPage() >> currentPage
+            CustomPagingNavigator customPaging = new CustomPagingNavigator("someId", pageable)
         when:
-            tester.startPage(new TestDataNavigationPage(10, 100))
+            tester.startComponentInPage(customPaging)
         then:
-            Component component = tester.getComponentFromLastRenderedPage("navigator:navigation:0")s
-
-            tester.assertModelValue("navigator:navigation:0", Model.of("active"))
+            Component activePageNumberItem = tester.getComponentFromLastRenderedPage("someId:navigation:$currentPage")
+            activePageNumberItem.getBehaviors(AttributeModifier).any {it.attribute == "class" && it.getReplaceModel() == Model.of("active")}
     }
 
+    @Override
+    protected void setupContext() {
+
+    }
 }
