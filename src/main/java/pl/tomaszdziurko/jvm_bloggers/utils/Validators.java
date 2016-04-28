12383a50c21cca78dbd05f93c104f481159773e2
Tomasz Dziurko <tdziurko@gmail.com>
2016-04-28 22:26:33 +0200

    Accept urls with double slashes as valid, fixes #202

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/utils/Validators.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/utils/Validators.java
@@ -1,6 +1,7 @@
 package pl.tomaszdziurko.jvm_bloggers.utils;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.util.StringUtils;
 
 public class Validators {
 
@@ -8,7 +9,30 @@ public class Validators {
         new UrlValidator(new String[]{"http", "https"});
 
     public static boolean isUrlValid(String url) {
-        return URL_VALIDATOR.isValid(url);
+
+        boolean isValid = URL_VALIDATOR.isValid(url);
+
+        // maybe it is the problem with 2nd '//' in address e.g. http://example.com//something
+        if (!isValid && oneNotNeededDoubleSlashIsPresent(url)) {
+            String urlWithoutDoubleSlash = reverseThenReplaceDoubleSlashThenReverseBack(url);
+            return URL_VALIDATOR.isValid(urlWithoutDoubleSlash);
+        }
+
+        return isValid;
+    }
+
+    private static String reverseThenReplaceDoubleSlashThenReverseBack(String url) {
+        String reversedUrlWithReplacedDoubleSlash = new StringBuilder(url)
+            .reverse()
+            .toString()
+            .replaceFirst("//", "/");
+        return new StringBuilder(reversedUrlWithReplacedDoubleSlash)
+            .reverse()
+            .toString();
+    }
+
+    private static boolean oneNotNeededDoubleSlashIsPresent(String url) {
+        return StringUtils.countOccurrencesOf(url, "//") == 2;
     }
 
 }
