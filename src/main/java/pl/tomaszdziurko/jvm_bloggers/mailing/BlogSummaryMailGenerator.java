e76b94820fe2821b457b38ad78241714e388b7bc
Tomasz Dziurko <tdziurko@gmail.com>
2016-08-25 23:13:22 +0200

    Modifies mail generation not to rely on new lines from mailing template sections, but instead of it to put them during e-mail creation.

--- src/main/java/pl/tomaszdziurko/jvm_bloggers/mailing/BlogSummaryMailGenerator.java
+++ src/main/java/pl/tomaszdziurko/jvm_bloggers/mailing/BlogSummaryMailGenerator.java
@@ -4,6 +4,7 @@ import com.google.common.base.Strings;
 
 import lombok.NoArgsConstructor;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.stringtemplate.v4.ST;
@@ -34,6 +35,8 @@ public class BlogSummaryMailGenerator {
     private static final char TEMPLATE_DELIMITER = '$';
     private static final String UTM_MEDIUM = "newsletter";
 
+    private static final String NEW_LINE = "<br/>";
+
     private MetadataRepository metadataRepository;
     private RedirectLinkGenerator linkGenerator;
 
@@ -48,16 +51,37 @@ public class BlogSummaryMailGenerator {
 
     public String prepareMailContent(NewsletterIssue newsletterIssue) {
         String greeting = getValueForSection(MetadataKeys.MAILING_GREETING);
-        String heading = getValueForSection(MetadataKeys.HEADING_TEMPLATE);
+        String heading = newsletterIssue.getHeading();
         String mainSection = prepareMainSectionWithBlogs(newsletterIssue);
-        String varia = getValueForSection(MetadataKeys.VARIA_TEMPLATE);
+        String varia = newsletterIssue.getVaria();
         String signature = getValueForSection(MetadataKeys.MAILING_SIGNATURE);
 
-        return greeting + heading + mainSection + varia + signature;
+        return appendNewLinesIfNotEmpty(greeting, 2)
+            + appendNewLinesIfNotEmpty(heading, 2)
+            + appendNewLinesIfNotEmpty(mainSection, 2)
+            + prepareVariaSection(varia, 2)
+            + NEW_LINE + signature;
     }
 
     private String getValueForSection(String key) {
         return metadataRepository.findByName(key).getValue();
+    }
+
+    private String appendNewLinesIfNotEmpty(String text, int numberOfNewLines) {
+        if (StringUtils.isEmpty(text)) {
+            return text;
+        } else {
+            return text + StringUtils.repeat(NEW_LINE, numberOfNewLines);
+        }
+    }
+
+    private String prepareVariaSection(String variaContent, int numberOfNewLines) {
+        if (StringUtils.isEmpty(variaContent)) {
+            return variaContent;
+        } else {
+            return "Varia:" + NEW_LINE + variaContent
+                + StringUtils.repeat(NEW_LINE, numberOfNewLines);
+        }
     }
 
     private String prepareMainSectionWithBlogs(NewsletterIssue newsletterIssue) {
