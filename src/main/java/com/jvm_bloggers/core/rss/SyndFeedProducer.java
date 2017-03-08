1af07bc293cf904a919c26f3cc8c843691125454
Tomasz Dziurko <tdziurko@gmail.com>
2017-03-08 17:08:02 +0100

    Adds support for zipped rss, adds unlimited JCE, fixes #334

--- src/main/java/com/jvm_bloggers/core/rss/SyndFeedProducer.java
+++ src/main/java/com/jvm_bloggers/core/rss/SyndFeedProducer.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableMap;
 import com.jvm_bloggers.core.data_fetching.http.ProtocolSwitchingAwareConnectionRedirectHandler;
 import com.jvm_bloggers.core.utils.Validators;
 import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import lombok.Cleanup;
@@ -12,12 +11,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
 import java.util.Optional;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipException;
 
 @Component
 @Slf4j
@@ -37,18 +39,31 @@ public class SyndFeedProducer {
                 ImmutableMap.of("User-Agent", FAKE_USER_AGENT);
             urlConnection = redirectHandler.handle(urlConnection, headers);
             @Cleanup
-            final InputStream inputStream = urlConnection.getInputStream();
+            final InputStream inputStream = wrapToGzipStreamIfNeeded(urlConnection.getInputStream());
             return Optional.of(new SyndFeedInput().build(new XmlReader(inputStream)));
-        } catch (IOException | FeedException ex) {
+        } catch (Exception ex) {
             log.warn("Error during fetching RSS {} url", rssUrl, ex);
             return Optional.empty();
         } finally {
             IOUtils.close(urlConnection);
         }
     }
-
+    
     public Optional<String> validUrlFromRss(String rss) {
         Optional<String> url = createFor(rss).map(SyndFeed::getLink);
         return url.filter(Validators::isUrlValid);
+    }
+
+    private InputStream wrapToGzipStreamIfNeeded(InputStream inputStream) throws IOException {
+        if (!inputStream.markSupported()) {
+            inputStream = new BufferedInputStream(inputStream);
+        }
+        inputStream.mark(1000);
+        try {
+            return new GZIPInputStream(inputStream);
+        } catch (ZipException e) {
+            inputStream.reset();
+            return inputStream;
+        }
     }
 }
