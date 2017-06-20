83e690d94a274bddda4e5022e864d0353cf88799
Tomasz Dziurko <tdziurko@gmail.com>
2017-06-20 18:43:16 +0200

    Moves social meta tags rendering to the application logic, closes #386

---
+++ src/main/java/com/jvm_bloggers/frontend/public_area/social_meta_data/SocialMetaData.java
@@ -0,0 +1,10 @@
+package com.jvm_bloggers.frontend.public_area.social_meta_data;
+
+public interface SocialMetaData {
+
+    String getTitle();
+
+    String getImageUrl();
+
+    String getDescription();
+}
