903282d9642bb76b5432c9ba0aff869478949676
Tomasz Dziurko <tdziurko@gmail.com>
2017-06-20 18:08:23 +0200

    Adds some tests for isEmpty() method

--- src/test/groovy/com/jvm_bloggers/utils/HtmlEmptinessCheckerSpec.groovy
+++ src/test/groovy/com/jvm_bloggers/utils/HtmlEmptinessCheckerSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Unroll
 class HtmlEmptinessCheckerSpec extends Specification {
 
     @Unroll
-    def "Should properly check for section emptiness when content = `#content`"() {
+    def "Should properly check for section isNotEmpty() result when content = `#content`"() {
 
         when:
         boolean isNotEmpty = HtmlEmptinessChecker.isNotEmpty(content)
@@ -27,6 +27,30 @@ class HtmlEmptinessCheckerSpec extends Specification {
         '<div>content</div>'              || true
         '<br>content<div>something</div>' || true
         '&nbsp; - item'                   || true
+    }
+
+    @Unroll
+    def "Should properly check for section isEmpty() result when content = `#content`"() {
+
+        when:
+        boolean isEmpty = HtmlEmptinessChecker.isEmpty(content)
+
+        then:
+        isEmpty == expectedValue
+
+        where:
+        content                           || expectedValue
+        ''                                || true
+        '  '                              || true
+        null                              || true
+        '<br>'                            || true
+        '<div><br></div>'                 || true
+        '&nbsp;'                          || true
+        '<br/>&nbsp;'                     || true
+        ' - content'                      || false
+        '<div>content</div>'              || false
+        '<br>content<div>something</div>' || false
+        '&nbsp; - item'                   || false
     }
 
 }
