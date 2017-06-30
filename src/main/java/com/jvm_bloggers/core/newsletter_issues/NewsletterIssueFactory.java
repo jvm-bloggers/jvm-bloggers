b173c36bfa879f26792b06c9915b05229efd24f9
Tomasz Dziurko <tdziurko@gmail.com>
2017-06-30 16:50:14 +0200

    Changes time when newsletter is published to 11:30
    Minor changes:
    - email frequency change from 4min to 2min to send all e-mails before midday
    - changed starting hour of posts inclusion in the newsletter
    - changed posts refreshing scheduler to it is executed 30mins before newsletter is generated

--- src/main/java/com/jvm_bloggers/core/newsletter_issues/NewsletterIssueFactory.java
+++ src/main/java/com/jvm_bloggers/core/newsletter_issues/NewsletterIssueFactory.java
@@ -52,7 +52,7 @@ public class NewsletterIssueFactory {
     private LocalDateTime calculateStartDate(int daysInThePastToIncludeInNewIssue) {
         return nowProvider.now()
             .minusDays(daysInThePastToIncludeInNewIssue)
-            .withHour(11)
+            .withHour(10)
             .withMinute(0)
             .withSecond(0)
             .withNano(0);
