4a39978a42d11123c9be398df9fa6ba641954e35
Tomasz Dziurko <tdziurko@gmail.com>
2017-07-06 18:16:21 +0200

    Store ip address, referer and user agent in message sent to ClickStoringActor

--- src/main/java/com/jvm_bloggers/core/blogpost_redirect/click_counter/SingleClick.java
+++ src/main/java/com/jvm_bloggers/core/blogpost_redirect/click_counter/SingleClick.java
@@ -4,6 +4,7 @@ import com.jvm_bloggers.entities.blog_post.BlogPost;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
 @RequiredArgsConstructor
 @Getter
@@ -11,5 +12,29 @@ public class SingleClick {
 
     @NonNull
     private final BlogPost blogPost;
+
+    @NonNull
+    private final Referer referer;
+
+    @NonNull
+    private final UserAgent userAgent;
+
+    @NonNull
+    private final IpAddress ip;
+
+    @Value
+    public static class Referer {
+        private final String value;
+    }
+
+    @Value
+    public static class UserAgent {
+        private final String value;
+    }
+
+    @Value
+    public static class IpAddress {
+        private final String value;
+    }
 
 }
