802c33f0bbc12a3065bc65474f261c24908d61a7
Lukasz Krauzowicz <kraluk@gmail.com>
2017-07-19 23:54:33 +0200

    Completed first implementation of the RSS info page, #375

--- src/main/java/com/jvm_bloggers/frontend/public_area/rss/RssInformationPage.java
+++ src/main/java/com/jvm_bloggers/frontend/public_area/rss/RssInformationPage.java
@@ -3,18 +3,33 @@ package com.jvm_bloggers.frontend.public_area.rss;
 import com.jvm_bloggers.frontend.public_area.AbstractFrontendPage;
 import com.jvm_bloggers.frontend.public_area.HomePage;
 
-import org.apache.wicket.markup.html.basic.Label;
+import lombok.Data;
+
+import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.wicketstuff.annotation.mount.MountPath;
+
+import java.util.Arrays;
 
 import static com.jvm_bloggers.core.rss.BlogPostsController.RSS_FEED_MAPPING;
 
 @MountPath("rss")
 public class RssInformationPage extends AbstractFrontendPage {
+    private static final String FEED_URL_PATTERN = "%s%s";
+
+    private final String feedUrl;
 
     public RssInformationPage() {
-        add(new Label("rss_feed", String.format("%s%s", getRootUrl(), RSS_FEED_MAPPING)));
+        this.feedUrl = String.format(FEED_URL_PATTERN, getRootUrl(), RSS_FEED_MAPPING);
+
+        add(new ExternalLink("rss_feed_url", feedUrl, feedUrl));
+
+        Arrays.stream(FeedUrl.values())
+            .forEach(e -> {
+                String url = getAbsoluteUrl(e.getParameter());
+                add(new ExternalLink(e.getId(), url, url));
+            });
     }
 
     @Override
@@ -25,5 +40,33 @@ public class RssInformationPage extends AbstractFrontendPage {
     private String getRootUrl() {
         return RequestCycle.get().getUrlRenderer().renderFullUrl(
             Url.parse(urlFor(HomePage.class, null).toString()));
+    }
+
+    private String getAbsoluteUrl(String parameter) {
+        return String.format(FEED_URL_PATTERN, feedUrl, parameter);
+    }
+
+    private enum FeedUrl {
+        XML("rss_feed_xml", ".xml"),
+        JSON("rss_feed_json", ".json"),
+        LIMIT("rss_feed_limit", "?limit=10"),
+        EXCLUDE_AUTHORS("rss_feed_exclude", "?excludedAuthors=Tomasz Dziurko,Adam Warski"),
+        COMPLEX("rss_feed_complex", ".json?limit=2&excludedAuthors=Tomasz Dziurko");
+
+        private final String id;
+        private final String parameter;
+
+        FeedUrl(String id, String parameter) {
+            this.id = id;
+            this.parameter = parameter;
+        }
+
+        String getId() {
+            return id;
+        }
+
+        String getParameter() {
+            return parameter;
+        }
     }
 }
