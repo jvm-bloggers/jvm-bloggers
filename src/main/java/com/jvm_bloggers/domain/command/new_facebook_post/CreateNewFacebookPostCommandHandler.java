ab473aa1702bc34f2e8c33902af9c14a2cf69075
Michal Wiacek <thepowerofra@gmail.com>
2017-07-25 23:19:31 +0200

    Simple Command Handler to creating new facebook post

--- src/main/java/com/jvm_bloggers/domain/command/new_facebook_post/CreateNewFacebookPostCommandHandler.java
+++ src/main/java/com/jvm_bloggers/domain/command/new_facebook_post/CreateNewFacebookPostCommandHandler.java
@@ -5,7 +5,9 @@ import com.jvm_bloggers.entities.fb.FacebookPost;
 import com.jvm_bloggers.entities.fb.FacebookPostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -16,9 +18,14 @@ public class CreateNewFacebookPostCommandHandler implements CommandHandler<Creat
     private final FacebookPostRepository facebookPostRepository;
 
     @Override
+    @EventListener
+    @Transactional
     public void handle(CreateNewFacebookPost command) {
 
+        FacebookPost facebookPost = new FacebookPost(
+        command.getIssueLink(),
+        command.getFacebookMessage());
 
-
+        facebookPostRepository.save(facebookPost);
     }
 }
