36f59c3237ea53b57392b7d7c4d402dcb136ca71
Michal Wiacek <thepowerofra@gmail.com>
2017-07-25 22:41:07 +0200

    New form for facebook post

--- src/main/java/com/jvm_bloggers/frontend/admin_area/AdminSocialChannelsPageBackingBean.java
+++ src/main/java/com/jvm_bloggers/frontend/admin_area/AdminSocialChannelsPageBackingBean.java
@@ -3,7 +3,6 @@ package com.jvm_bloggers.frontend.admin_area;
 
 import com.jvm_bloggers.domain.command.CommandPublisher;
 import com.jvm_bloggers.domain.command.new_facebook_post.CreateNewFacebookPost;
-import com.jvm_bloggers.entities.fb.FacebookPost;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
