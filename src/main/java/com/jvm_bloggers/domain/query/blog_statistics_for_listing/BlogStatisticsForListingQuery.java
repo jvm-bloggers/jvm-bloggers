e9e51de94d6b64604e6d9ac1fd47bb82b5afc4a0
Tomasz Dziurko <tdziurko@gmail.com>
2017-07-26 18:41:50 +0200

    Uses NowProvider as a ... date-time provider :)

--- src/main/java/com/jvm_bloggers/domain/query/blog_statistics_for_listing/BlogStatisticsForListingQuery.java
+++ src/main/java/com/jvm_bloggers/domain/query/blog_statistics_for_listing/BlogStatisticsForListingQuery.java
@@ -3,6 +3,7 @@ package com.jvm_bloggers.domain.query.blog_statistics_for_listing;
 import com.jvm_bloggers.entities.blog.BlogRepository;
 import com.jvm_bloggers.entities.blog.BlogType;
 
+import com.jvm_bloggers.utils.NowProvider;
 import javaslang.collection.List;
 import lombok.AllArgsConstructor;
 
@@ -22,13 +23,14 @@ public class BlogStatisticsForListingQuery {
     public static final String BLOG_STATISTICS_CACHE = "Blog posts statistics cache";
 
     private final BlogRepository blogRepository;
+    private final NowProvider nowProvider;
 
     @Cacheable(BLOG_STATISTICS_CACHE)
     public List<BlogStatisticsForListing> findBlogPostStatistics(BlogType blogType,
                                                                  int page, int size) {
         return blogRepository.findBlogStatistics(
-            LocalDate.now().minusMonths(3).atStartOfDay(),
-            LocalDate.now().minusMonths(12).atStartOfDay(),
+            nowProvider.today().minusMonths(3).atStartOfDay(),
+            nowProvider.today().minusMonths(12).atStartOfDay(),
             blogType, new PageRequest(page, size))
             .map(BlogStatisticsForListing::fromBlogPostStatisticProjection);
     }
