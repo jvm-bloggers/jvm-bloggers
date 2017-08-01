ac7fa96e05b2499200f9807e76de458d138fb17e
Michal Chmielarz <michal.chmielarz@gmail.com>
2017-08-01 08:40:52 +0200

    Correct order of imports

--- src/main/java/com/jvm_bloggers/core/social/twitter/publisher/TwitterPublishingScheduler.java
+++ src/main/java/com/jvm_bloggers/core/social/twitter/publisher/TwitterPublishingScheduler.java
@@ -5,13 +5,13 @@ import com.jvm_bloggers.entities.twitter.Tweet;
 import com.jvm_bloggers.entities.twitter.TweetRepository;
 import com.jvm_bloggers.utils.NowProvider;
 
+import io.vavr.control.Option;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import io.vavr.control.Option;
 
 import static lombok.AccessLevel.PACKAGE;
 
