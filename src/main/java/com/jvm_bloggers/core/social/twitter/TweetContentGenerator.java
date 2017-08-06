1758f2e500fb1b97093815240be05662f55ef0bc
Michal Chmielarz <michal.chmielarz@gmail.com>
2017-08-06 19:39:05 +0200

    Vavr, fluent API and shuffling

--- src/main/java/com/jvm_bloggers/core/social/twitter/TweetContentGenerator.java
+++ src/main/java/com/jvm_bloggers/core/social/twitter/TweetContentGenerator.java
@@ -1,18 +1,18 @@
 package com.jvm_bloggers.core.social.twitter;
 
 import com.jvm_bloggers.core.blogpost_redirect.LinkGenerator;
+import com.jvm_bloggers.entities.blog.Blog;
+import com.jvm_bloggers.entities.blog_post.BlogPost;
 import com.jvm_bloggers.entities.newsletter_issue.NewsletterIssue;
+
+import io.vavr.collection.List;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Random;
-
 import static java.lang.String.format;
-import static java.util.stream.Collectors.toList;
 import static lombok.AccessLevel.PACKAGE;
 
 @Component
@@ -27,36 +27,39 @@ class TweetContentGenerator {
         "Nowy numer #%s już online - %s z postami m.in. %s i %s #java #jvm";
 
     private final LinkGenerator linkGenerator;
-    private final Random random = new Random();
 
     public String generateTweetContent(NewsletterIssue issue) {
         final List<String> personalTTs =
-            issue.getBlogPosts().stream().map(b -> b.getBlog()).filter(b -> b.isPersonal())
-                .map(b -> b.getTwitter()).collect(toList());
-        final List<String> companyTTs =
-            issue.getBlogPosts().stream().map(b -> b.getBlog()).filter(b -> !b.isPersonal())
-                .map(b -> b.getTwitter()).collect(toList());
+            List.ofAll(issue.getBlogPosts())
+                .map(BlogPost::getBlog)
+                .filter(Blog::isPersonal)
+                .map(Blog::getTwitter)
+                .shuffle()
+                .take(2);
 
-        final String personalHandle1 = personalTTs.remove(random.nextInt(personalTTs.size()));
-        final String personalHandle2 = personalTTs.remove(random.nextInt(personalTTs.size()));
-        final String companyHandle = companyTTs.remove(random.nextInt(companyTTs.size()));
+        final String companyTT =
+            List.ofAll(issue.getBlogPosts())
+                .map(BlogPost::getBlog)
+                .filter(b -> !b.isPersonal())
+                .map(Blog::getTwitter)
+                .shuffle()
+                .head();
 
         final String issueLink = linkGenerator.generateIssueLink(issue.getIssueNumber());
         final String tweetContent =
             format(
                 MESSAGE_TEMPLATE, issue.getIssueNumber(), issueLink,
-                personalHandle1, companyHandle, personalHandle2
+                personalTTs.get(0), companyTT, personalTTs.get(1)
             );
 
         if (tweetIsTooLong(tweetContent, issueLink.length())) {
             return format(
                 SHORT_MESSAGE_TEMPLATE, issue.getIssueNumber(), issueLink,
-                personalHandle1, companyHandle
+                personalTTs.get(0), companyTT
             );
         } else {
             return tweetContent;
         }
-        // check for length - link 23 chars, number 3 chars
     }
 
     private boolean tweetIsTooLong(String tweetContent, int originalIssuesLinkLength) {
