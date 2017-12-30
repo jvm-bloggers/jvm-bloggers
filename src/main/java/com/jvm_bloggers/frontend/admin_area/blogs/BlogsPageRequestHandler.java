ef6d756f8e8b2145a6f1c20263152a78b719a1ce
Grzegorz Sancewicz <g.sancewicz@gmail.com>
2017-12-30 16:46:04 +0100

    Add sorting to blog list in the admin panel, closes #442

--- src/main/java/com/jvm_bloggers/frontend/admin_area/blogs/BlogsPageRequestHandler.java
+++ src/main/java/com/jvm_bloggers/frontend/admin_area/blogs/BlogsPageRequestHandler.java
@@ -1,33 +1,42 @@
 package com.jvm_bloggers.frontend.admin_area.blogs;
 
+import com.jvm_bloggers.core.utils.WicketToSpringSortingConverter;
 import com.jvm_bloggers.entities.blog.Blog;
 import com.jvm_bloggers.entities.blog.BlogRepository;
 import com.jvm_bloggers.frontend.admin_area.PaginationConfiguration;
 
-import lombok.AllArgsConstructor;
-
-import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.injection.Injector;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Component;
 
 import java.util.Iterator;
 
-@Component
-@AllArgsConstructor
-public class BlogsPageRequestHandler implements IDataProvider<Blog> {
+public class BlogsPageRequestHandler extends SortableDataProvider<Blog, String> {
 
+    @SpringBean
     private PaginationConfiguration paginationConfiguration;
 
+    @SpringBean
     private BlogRepository blogRepository;
+
+    BlogsPageRequestHandler() {
+        Injector.get().inject(this);
+        setSort(new SortParam<>("author", true));
+    }
 
     @Override
     public Iterator<? extends Blog> iterator(long first, long count) {
         int page = Long.valueOf(first / paginationConfiguration.getDefaultPageSize()).intValue();
+
         return blogRepository
-                .findAllByOrderByAuthorAsc(new PageRequest(page,
-                        paginationConfiguration.getDefaultPageSize())
-                ).iterator();
+            .findAll(new PageRequest(page,
+                paginationConfiguration.getDefaultPageSize(),
+                WicketToSpringSortingConverter.convert(getSort()).getOrNull())
+            )
+            .iterator();
     }
 
     @Override
--- src/test/groovy/com/jvm_bloggers/frontend/admin_area/blogs/BlogsPageSpec.groovy
+++ src/test/groovy/com/jvm_bloggers/frontend/admin_area/blogs/BlogsPageSpec.groovy
@@ -6,57 +6,94 @@ import com.jvm_bloggers.entities.blog.BlogRepository
 import com.jvm_bloggers.entities.blog.BlogType
 import com.jvm_bloggers.entities.blog_post.BlogPostRepository
 import com.jvm_bloggers.frontend.admin_area.PaginationConfiguration
+import org.apache.wicket.util.tester.TagTester
+import org.springframework.data.domain.PageImpl
 
-import static com.jvm_bloggers.frontend.admin_area.blogs.BlogsPage.BLOGS_DATA_VIEW_ID
-import static com.jvm_bloggers.frontend.admin_area.blogs.BlogsPage.BLOG_DATA_FORM_ID
-import static com.jvm_bloggers.frontend.admin_area.blogs.BlogsPage.BLOG_POSTS_LINK_ID
+import static com.jvm_bloggers.frontend.admin_area.blogs.BlogsPage.*
 import static java.time.LocalDateTime.now
 
 class BlogsPageSpec extends MockSpringContextAwareSpecification {
 
-    PaginationConfiguration paginationConfiguration = new PaginationConfiguration(15);
-    BlogsPageRequestHandler requestHandler = Mock(BlogsPageRequestHandler.class)
+    PaginationConfiguration paginationConfiguration = new PaginationConfiguration(15)
+    BlogRepository blogRepository = Mock(BlogRepository.class)
 
     @Override
     protected void setupContext() {
         addBean(paginationConfiguration)
-        addBean(requestHandler)
-        addBean(Mock(BlogRepository.class))
+        addBean(blogRepository)
         addBean(Mock(BlogPostRepository.class))
     }
 
     def "Should redirect to blog's posts page"() {
         given:
-        Blog blog = sampleBlog()
-        requestHandler.iterator(_, _) >> [blog].iterator()
-        requestHandler.size() >> 1
-        requestHandler.model(_) >> new BlogModel(blog)
+            Blog blog = sampleBlog(1)
+            blogRepository.findAll(_) >> new PageImpl<>([blog])
+            blogRepository.count() >> 1
 
         when:
-        tester.startPage(BlogsPage.class)
-        tester.clickLink(linkPath(BLOG_POSTS_LINK_ID))
+            tester.startPage(BlogsPage.class)
+            tester.clickLink(linkPath(1, 7, POSTS_LINK))
 
         then:
-        tester.assertRenderedPage(BlogPostsPage.class)
+            tester.assertRenderedPage(BlogPostsPage.class)
     }
 
-    private Blog sampleBlog() {
+    def "Should be ordered by author asc"() {
+        given:
+            Blog blog1 = sampleBlog(1)
+            Blog blog2 = sampleBlog(2)
+            blogRepository.findAll(_) >> new PageImpl<>([blog1, blog2])
+            blogRepository.count() >> 2
+
+        when:
+            tester.startPage(BlogsPage.class)
+
+        then:
+            "Author" == getSortedColumnHeader("wicket_orderUp")
+    }
+
+    def "Should be ordered by author desc"() {
+        given:
+            Blog blog1 = sampleBlog(1)
+            Blog blog2 = sampleBlog(2)
+            blogRepository.findAll(_) >> new PageImpl<>([blog1, blog2])
+            blogRepository.count() >> 2
+
+        when:
+            tester.startPage(BlogsPage.class)
+            tester.clickLink(headerLinkPath(1))
+
+        then:
+            "Author" == getSortedColumnHeader("wicket_orderDown")
+    }
+
+    private String getSortedColumnHeader(String sortingClass) {
+        TagTester authorHeaderTester = TagTester.createTagByAttribute(
+                tester.getLastResponse().getDocument(), "class", sortingClass)
+        return authorHeaderTester.getChild("a").getChild("span").getValue()
+    }
+
+    private Blog sampleBlog(int id) {
         Blog.builder()
-            .id(1)
-            .bookmarkableId("bookmarkableId")
-            .author("some author")
-            .rss("some rss")
-            .url("some url")
-            .twitter("some twitter")
-            .dateAdded(now())
-            .blogType(BlogType.PERSONAL)
-            .active(true)
-            .moderationRequired(false)
-            .build()
+                .id(id)
+                .bookmarkableId("bookmarkableId " + id)
+                .author("some author " + id)
+                .rss("some rss " + id)
+                .url("some url")
+                .twitter("some twitter")
+                .dateAdded(now())
+                .blogType(BlogType.PERSONAL)
+                .active(true)
+                .moderationRequired(false)
+                .build()
     }
 
-    private def String linkPath(String buttonId) {
-        [BLOG_DATA_FORM_ID, BLOGS_DATA_VIEW_ID, 1, buttonId].join(":")
+    private String headerLinkPath(int column) {
+        [BLOG_DATA_FORM_ID, BLOGS_DATA_TABLE_ID, "topToolbars:toolbars:1:headers", column, "header:orderByLink"].join(":")
+    }
+
+    private String linkPath(int row, int column, String... ids) {
+        [BLOG_DATA_FORM_ID, BLOGS_DATA_TABLE_ID, "body:rows", row, "cells", column, "cell", ids].flatten().join(":")
     }
 
 }
