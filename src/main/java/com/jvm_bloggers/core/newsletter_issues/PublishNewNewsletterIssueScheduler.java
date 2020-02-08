61d8aef914fe8166bff7076dd451e63c41167f50
Tomasz Dziurko <tomasz.dziurko@softwaremill.com>
2020-02-08 20:09:59 +0100

    Fixes bug when posts accepted on Friday after midnight were included in two newsletters. #fixes 535

--- src/main/java/com/jvm_bloggers/core/newsletter_issues/PublishNewNewsletterIssueScheduler.java
+++ src/main/java/com/jvm_bloggers/core/newsletter_issues/PublishNewNewsletterIssueScheduler.java
@@ -1,17 +1,16 @@
 package com.jvm_bloggers.core.newsletter_issues;
 
+import static com.jvm_bloggers.core.newsletter_issues.PublishingConstants.DAYS_IN_THE_PAST_TO_INCLUDE_IN_NEW_ISSUE;
+import static com.jvm_bloggers.utils.NowProvider.DEFAULT_ZONE_NAME;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import static com.jvm_bloggers.utils.NowProvider.DEFAULT_ZONE_NAME;
-
 @Component
 @Slf4j
 public class PublishNewNewsletterIssueScheduler {
-
-    private static final int DAYS_IN_THE_PAST_TO_INCLUDE_IN_NEW_ISSUE = 7;
 
     private final NewNewsletterIssuePublisher newIssuePublisher;
 
