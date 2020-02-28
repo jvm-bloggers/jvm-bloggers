b16d830ae90ed2085c2bd5a58b45e1c8e98a79b1
krzysztof kepinski <krzysztof.kepinski@airhelp.com>
2020-02-28 23:26:31 +0100

    Introduce ObjectMother class that generates common data for Blog and BlogPost test objects. Easy data customisation is allowed by passing optional Map parameter to factory methods. Example usage shown on two tests: BlogPostRepositorySpec and BlogSpec

--- src/test/groovy/com/jvm_bloggers/entities/blog/BlogSpec.groovy
+++ src/test/groovy/com/jvm_bloggers/entities/blog/BlogSpec.groovy
@@ -1,9 +1,10 @@
 package com.jvm_bloggers.entities.blog
 
-import com.jvm_bloggers.utils.NowProvider
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Unroll
+
+import static com.jvm_bloggers.ObjectMother.aBlog
 
 @Subject(Blog)
 class BlogSpec extends Specification {
@@ -11,15 +12,7 @@ class BlogSpec extends Specification {
     @Unroll
     def "Should return approvedValue equal to #expectedInitialApprovedValue when moderationRequiredValue is #moderationRequiredValue"() {
         given:
-        Blog blog = Blog.builder()
-            .bookmarkableId("john-doe")
-            .author("John Doe")
-            .rss("http://john-doe.com/feed/")
-            .url("url")
-            .dateAdded(new NowProvider().now())
-            .blogType(BlogType.PERSONAL)
-            .moderationRequired(moderationRequiredValue)
-            .build()
+        Blog blog = aBlog(moderationRequired: moderationRequiredValue)
 
         when:
         Boolean approvedValue = blog.getInitialApprovedValue()
