f2a5fd9f61aff3be90592bac08786694d0d4d9bc
Łukasz Krauzowicz <kraluk@gmail.com>
2022-09-04 22:52:37 +0200

    Reformat the code
    closes jvm-bloggers#639

--- src/main/java/com/jvm_bloggers/core/data_fetching/blog_posts/BlogPostFactory.java
+++ src/main/java/com/jvm_bloggers/core/data_fetching/blog_posts/BlogPostFactory.java
@@ -3,6 +3,7 @@ package com.jvm_bloggers.core.data_fetching.blog_posts;
 import com.jvm_bloggers.core.utils.EmojiRemover;
 import com.jvm_bloggers.entities.blog.Blog;
 import com.jvm_bloggers.entities.blog_post.BlogPost;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,13 +27,13 @@ class BlogPostFactory {
         final var approved = blog.getInitialApprovedValue();
 
         return BlogPost.builder()
-                .title(remover.remove(title))
-                .url(url)
-                .publishedDate(publishedDate)
-                .approvedDate(getApprovedDate(approved, publishedDate))
-                .approved(approved)
-                .blog(blog)
-                .build();
+            .title(remover.remove(title))
+            .url(url)
+            .publishedDate(publishedDate)
+            .approvedDate(getApprovedDate(approved, publishedDate))
+            .approved(approved)
+            .blog(blog)
+            .build();
     }
 
     private LocalDateTime getApprovedDate(Boolean approved, LocalDateTime publishedDate) {
--- src/main/java/com/jvm_bloggers/core/utils/EmojiRemover.java
+++ src/main/java/com/jvm_bloggers/core/utils/EmojiRemover.java
@@ -1,7 +1,9 @@
 package com.jvm_bloggers.core.utils;
 
 import com.vdurmont.emoji.EmojiParser;
+
 import io.vavr.control.Option;
+
 import org.apache.commons.lang3.StringUtils;
 
 import static org.apache.commons.lang3.StringUtils.EMPTY;
@@ -10,8 +12,8 @@ public class EmojiRemover {
 
     public String remove(final String input) {
         return Option.of(input)
-                .filter(StringUtils::isNotBlank)
-                .map(EmojiParser::removeAllEmojis)
-                .getOrElse(EMPTY);
+            .filter(StringUtils::isNotBlank)
+            .map(EmojiParser::removeAllEmojis)
+            .getOrElse(EMPTY);
     }
 }
