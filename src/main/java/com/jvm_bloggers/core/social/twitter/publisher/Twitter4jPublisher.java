b0d6933f2e2cecba17110f67026b9dead05ed7ec
Łukasz Krauzowicz <kraluk@gmail.com>
2022-12-18 01:20:18 +0100

    Code polishing

--- src/main/java/com/jvm_bloggers/core/social/twitter/publisher/Twitter4jPublisher.java
+++ src/main/java/com/jvm_bloggers/core/social/twitter/publisher/Twitter4jPublisher.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import twitter4j.v1.TwitterV1;
+
 import static com.jvm_bloggers.ApplicationProfiles.PRODUCTION;
 import static com.jvm_bloggers.core.social.twitter.publisher.TwitterPublisher.TwitterPublishingStatus.ERROR;
 import static com.jvm_bloggers.core.social.twitter.publisher.TwitterPublisher.TwitterPublishingStatus.SUCCESS;
@@ -24,14 +26,14 @@ class Twitter4jPublisher implements TwitterPublisher {
     private final TwitterClientFactory clientFactory;
 
     @Override
-    public TwitterPublishingStatus publish(Tweet tweet) {
+    public TwitterPublishingStatus publish(final Tweet tweet) {
         return Try.of(clientFactory::getClient)
-            .mapTry(twitter -> twitter.tweets().updateStatus(tweet.getContent()))
+            .mapTry(TwitterV1::tweets)
+            .mapTry(tweets -> tweets.updateStatus(tweet.getContent()))
             .onSuccess(status -> log.info("Tweet published successfully {}", status))
             .map(status -> SUCCESS)
             .onFailure(ex -> log.error("Cannot publish a tweet", ex))
             .recover(ex -> ERROR)
             .get();
     }
-
 }
