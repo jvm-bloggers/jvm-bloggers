f1dcab179f256a80df71bd4449c4e07175d110a5
malochak <konrad_malocha@hotmail.com>
2023-04-01 20:02:10 +0200

    #582 - implement update call whenever rss is fetched and parsed correctly

--- src/main/java/com/jvm_bloggers/entities/blog/Blog.java
+++ src/main/java/com/jvm_bloggers/entities/blog/Blog.java
@@ -1,6 +1,19 @@
 package com.jvm_bloggers.entities.blog;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+
 import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,16 +21,6 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
-import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.Indexed;
 
 @Entity
 @Table(name = "blog")
@@ -80,6 +83,9 @@ public class Blog {
 
     @Column(name = "MODERATION_REQUIRED", nullable = false)
     private Boolean moderationRequired;
+
+    @Column(name = "DATE_RSS_LAST_FETCHED")
+    private LocalDateTime dateLastRssFetched;
 
     public Boolean isModerationRequired() {
         return moderationRequired;
